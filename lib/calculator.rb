de27bfe4e185d96ca5ff4f00250a10504fc4f51a
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-02-01 20:05:30 +0300

    write #subtract, #multiply, #divide functions and pass tests

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,4 +2,16 @@ class Calculator
     def add(*args)
         args.inject(0) { |sum, item| sum + item }
     end
+
+    def subtract(a,b)
+        a - b
+    end
+
+    def multiply(*args)
+        args.inject(1) { |sum, item| sum * item }
+    end
+
+    def divide(a, b)
+        a / b
+    end
 end
