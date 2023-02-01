d27fbf1b0d2c5015b94b10aa7daa5850bfc4cc0b
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-02-01 20:01:37 +0300

    write new tests for #subtract, #multiply, #divide methods

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -12,4 +12,30 @@ describe Calculator do
         expect(calculator.add(2, 5, 7)).to eql(14)
       end
     end
+
+    describe "#subtract" do
+        it "returns the subtaction of two numbers" do
+          calculator = Calculator.new
+          expect(calculator.subtract(0, -1)).to eql(1)
+        end
+    end
+
+    describe "#multiply" do
+        it "returns the multiplication of two numbers" do 
+            calculator = Calculator.new
+            expect(calculator.multiply(1, 0)).to eql(0)
+        end
+        
+        it "returns the multiplication of more than two numbers" do 
+            calculator = Calculator.new
+            expect(calculator.multiply(1, 1, 4)).to eql(0)
+        end
+    end
+
+    describe "#divide" do
+        it "returns division of two numbers" do
+            calculator = Calculator.new
+            expect(calculator.divide(5, 5)).to eql(1)
+        end
+    end
 end
