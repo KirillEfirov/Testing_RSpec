ca5ff5d8bb8fea855a5975071e9c617c058439cd
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-02-03 23:59:04 +0300

    Write tests for #caesar_cipher method

---
+++ spec/caesar_cipher_spec.rb
@@ -0,0 +1,21 @@
+require "./lib/caesar_cipher.rb"
+
+describe "#caesar_cipher" do
+    it "should return encrypted word" do
+        encrypted = caesar_cipher "WORD", 5
+        
+        expect(encrypted).to eql("Btwi")
+    end
+
+    it "should return encrypted word" do
+        encrypted = caesar_cipher "aBcD", 1
+        
+        expect(encrypted).to eql("Bcde")
+    end
+
+    it "should return encrypted word" do
+        encrypted = caesar_cipher "word", 0
+        
+        expect(encrypted).to eql("Word")
+    end
+end
