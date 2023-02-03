473c2d42d07f614ce6d2640aabe5a3e3c6e56804
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-02-04 00:20:49 +0300

    Add tests for #caesar_cipher method

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,19 +1,19 @@
 require "./lib/caesar_cipher.rb"
 
 describe "#caesar_cipher" do
-    it "should return encrypted word" do
+    it "should return encrypted word 'Btwi'" do
         encrypted = caesar_cipher "WORD", 5
         
         expect(encrypted).to eql("Btwi")
     end
 
-    it "should return encrypted word" do
+    it "should return encrypted word 'Bcde'" do
         encrypted = caesar_cipher "aBcD", 1
         
         expect(encrypted).to eql("Bcde")
     end
 
-    it "should return encrypted word" do
+    it "should return encrypted word 'Word'" do
         encrypted = caesar_cipher "word", 0
         
         expect(encrypted).to eql("Word")
@@ -29,5 +29,11 @@ describe "#caesar_cipher" do
         encrypted = caesar_cipher "    ", 0
         
         expect(encrypted).to eql("")
+    end
+
+    it "should return empty 'C'" do
+        encrypted = caesar_cipher " b   ", 1
+        
+        expect(encrypted).to eql("C")
     end
 end
