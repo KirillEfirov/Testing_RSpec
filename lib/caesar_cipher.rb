07d908ac7c525ae967a75cd770bd45f2ae937bb0
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-02-04 00:04:20 +0300

    Rewrite a method #caesar_cipher to pass the tests

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -2,8 +2,10 @@ def caesar_cipher word, shift
     arr = ('A'..'Z').to_a
     newWord = Array.new
 
+    word = word.split("")
+
     word.each do |letter|
-        index = arr.index(letter)
+        index = arr.index(letter.capitalize)
 
         if ((index + shift) > (arr.length - 1))
             newWord.push(arr[shift - (arr.length - 1 - index) - 1])
@@ -12,8 +14,6 @@ def caesar_cipher word, shift
         else newWord.push(arr[index + shift])
         end
     end
-    p newWord.join()
+    p newWord.join().capitalize
 
 end
-
-CaesarCipher ["W", "O", "R", "D"], 5
