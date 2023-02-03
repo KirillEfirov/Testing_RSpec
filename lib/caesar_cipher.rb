878726baa41a17255987acc8f50d713e68735ec9
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-02-04 00:24:39 +0300

    Refactor code

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -5,12 +5,13 @@ def caesar_cipher word, shift
     word.each_char do |letter|
         index = arr.index(letter.capitalize)
 
-        if letter == " "
-            next
-        elsif ((index + shift) > (arr.length - 1))
+        next if letter == " "
+    
+        if ((index + shift) > (arr.length - 1))
             newWord.push(arr[shift - (arr.length - 1 - index) - 1])
         else newWord.push(arr[index + shift])
         end
     end
-    p newWord.join().capitalize
+    
+    newWord.join().capitalize
 end
